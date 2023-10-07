7007359096a58c210a007cee8967956afdb3d262
Fred <druetfrederique27@gmail.com>
2023-10-07 17:00:21 +0200

    ChangePokemon

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,12 +1,29 @@
-
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 function PokemonCard() {
-    return (
-    <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt = "Bulbasaur" />
-        <figcaption>Bulbasaur</figcaption>
-    </figure>
-    );
+  const pokemon = pokemonList[1];
+
+  const image = pokemon.imgSrc ? (
+    <img src={pokemon.imgSrc} alt={pokemon.name} />
+  ) : (
+    <p>???</p>
+  );
+
+  return (
+    <div>
+      <h2>{pokemon.name}</h2>
+      {image}
+    </div>
+  );
 }
 
 export default PokemonCard;
