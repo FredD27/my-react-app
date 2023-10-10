0e26d752ccaaa688e4f21c108004f692fe0a6c06
Fred <druetfrederique27@gmail.com>
2023-10-10 16:54:05 +0200

    addbuttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,63 @@
-
-import './App.css'
-import PokemonCard from './components/PokemonCard';
+import { useState } from "react";
+import "./App.css";
+import PokemonCard from "./components/PokemonCard";
 
 const pokemonList = [
   {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
 function App() {
-  const selectedPokemon = pokemonList[0];
+  const [pokemonIndex, setpokemonIndex] = useState(0);
+
+  const handleClick = () => {
+    if (pokemonIndex < pokemonList.length - 1) 
+    setpokemonIndex(pokemonIndex + 1);
+  }
+
+  const lessClick = () => {
+    if (pokemonIndex > 0) 
+    setpokemonIndex(pokemonIndex - 1);
+  }
+
   return (
+
     <div>
-      <PokemonCard pokemon={selectedPokemon}/>
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+      {pokemonIndex > 0 && <button onClick={lessClick}>Précédent</button>}
+      {pokemonIndex < 4 && <button onClick={handleClick}>Suivant</button>}
     </div>
   );
 }
 
 export default App;
+
+// const handleClick = () => {
+//   setCount(count + 1)
+// }
+
+// return (<div>
+//   <p>{count}</p>
+//   <button onClick={handleClick}>Click</button>
+// </div>);
